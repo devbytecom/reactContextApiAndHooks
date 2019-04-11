efa618a6b5bf050736620794da9328d7d4fcedea
Shaun Brown <shaun.brown@devbyte.com>
2019-04-11 08:13:38 +0100

    Added multiple reducers and ability to filter down to each

--- src/components/componentWithState.js
+++ src/components/componentWithState.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { useStateValue } from '../context/stateExample';
 
 const Potato = () => {
-    const [{ test, count }, dispatch] = useStateValue();
-    console.log(test, dispatch);
+    const [{ component }, dispatch] = useStateValue();
 
     const action = () => {
         dispatch({ type: 'SOMETHING' });
@@ -14,8 +13,8 @@ const Potato = () => {
         <div>
             <h1>component with state....</h1>
             <h2 onClick={action}>Click me...</h2>
-            <p>VALUE IS...{test.toString()}</p>
-            <p>Click count IS...{count.toString()}</p>
+            <p>VALUE IS...{component.test.toString()}</p>
+            <p>Click count IS...{component.count.toString()}</p>
         </div>
     );
 };
--- src/components/container.js
+++ src/components/container.js
@@ -1,30 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ComponentWithState from './componentWithState';
 import DumbComponent from './dumbComponent';
 import { StateProvider } from '../context/stateExample';
-
-const initialState = { test: false, count: 0 };
-
-const exampleReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case 'SOMETHING':
-            return {
-                ...state,
-                test: true
-            };
-        case 'ADD':
-            return {
-                ...state,
-                count: state.count+1
-            };
-        default:
-            return state;
-    }
-};
+import mainReducer, { mainInitialState } from '../reducers';
 
 const Container = () => {
     return (
-        <StateProvider initialState={initialState} reducer={exampleReducer}>
+        <StateProvider initialState={mainInitialState} reducer={mainReducer}>
             <ComponentWithState />
             <DumbComponent />
         </StateProvider>
